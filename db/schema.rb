91d69678e13d8b76f704f941439faecda0a73ace
komaku-program <t.hirabayashi0430@gmail.com>
2023-09-05 06:40:17 +0900

    add user_id to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_04_062620) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_04_213644) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "thumbnail"
+    t.integer "user_id", default: 1
   end
 
   create_table "users", force: :cascade do |t|
