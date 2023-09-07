a892a1eb010dc866a72e99c5102ce66f7715c16f
komaku-program <t.hirabayashi0430@gmail.com>
2023-09-07 21:33:53 +0900

    edit session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,5 @@
 Rails.application.config.session_store :cookie_store,
                                        key: '_your_app_session',
-                                       secure: Rails.env.production?,
                                        same_site: :none,
-                                       expire_after: 14.days,
-                                       domain: :all
+                                       secure: true,
+                                       expire_after: 14.days
