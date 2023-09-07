18c919e8dd3cd89617479ed4392caa3f03d1da81
komaku-program <t.hirabayashi0430@gmail.com>
2023-09-07 20:58:04 +0900

    add domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,5 @@ Rails.application.config.session_store :cookie_store,
                                        key: '_your_app_session',
                                        secure: Rails.env.production?,  # HTTPSを使用する場合はこのフラグをtrueに設定
                                        same_site: :lax,  # オプションは :strict, :lax, :none から選べます
-                                       expire_after: 14.days
+                                       expire_after: 14.days,
+                                       domain: :all
