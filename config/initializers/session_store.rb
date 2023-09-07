6599aff15d160e822e16eddc690726457ca0c6ee
komaku-program <t.hirabayashi0430@gmail.com>
2023-09-07 22:54:29 +0900

    complete session program

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,5 +3,3 @@ Rails.application.config.session_store :cookie_store,
                                        same_site: :none,
                                        secure: true,
                                        expire_after: 14.days
-
-# Rails.application.config.action_dispatch.cookies_same_site_protection = :none
